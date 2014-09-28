8e6e568d4c7a40bb7e1d4c9d7f25eabf0fde846b
Bruno Dupuis <bruno.dupuis@novapost.net>
2014-09-28 22:40:33 +0200

    added a simpole django middleware

---
+++ batchloghandler/django_utils.py
@@ -0,0 +1,30 @@
+import logging
+import batchloghandler
+
+LOGGER_NAMES = []
+
+def reset_logger_list():
+    global LOGGER_NAMES
+    from django.conf import settings
+    loggers = settings.LOGGING.get('loggers', {})
+    LOGGER_NAMES = loggers.keys()
+
+
+class Middleware(object):
+
+    def __init__(self):
+        reset_logger_list()
+
+    def process_request(self, request):
+        for name in LOGGER_NAMES:
+            batchloghandler.record(name)
+
+    def process_exception(self, request, exception):
+        for name in LOGGER_NAMES:
+            batchloghandler.stop(name)
+
+    def process_response(self, request, response):
+        for name in LOGGER_NAMES:
+            batchloghandler.stop(name)
+        return response
+
